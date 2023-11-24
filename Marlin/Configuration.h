b3ce2f22b3d37946d80fa015db8ff15869da81c9
Mary <82013496+MaryLeoGD@users.noreply.github.com>
2023-11-25 00:47:05 +0200

    .last

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -64,16 +64,18 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Mary, Ender-3 V1)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Mary, Ender-3 V1.1)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-   #define MOTHERBOARD BOARD_MELZI
-   //#define MOTHERBOARD BOARD_SANGUINOLOLU_12
-  //#define MOTHERBOARD BOARD_MELZI_CREALITY
+     #define MOTHERBOARD BOARD_SANGUINOLOLU_12    // Sanguinololu 1.2 and above
+   //#define MOTHERBOARD BOARD_MELZI                 // Melzi     write No work
+   //#define MOTHERBOARD BOARD_MELZI_V2              // Melzi V2    write No work
+   //#define MOTHERBOARD BOARD_MELZI_CREALITY        // Melzi Creality3D (for CR-10 etc) No work
+    //#define MOTHERBOARD BOARD_MELZI_CREALITY_ENDER2 // Melzi Creality3D (for Ender-2) No work
 #endif
 
 /**
@@ -130,7 +132,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 V1"
+#define CUSTOM_MACHINE_NAME "Ender-3 V1.1"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
