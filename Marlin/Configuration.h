8a0c915bd2ca7f8c3a6fb2b23fbacdd3ea834dc1
Mary <82013496+MaryLeoGD@users.noreply.github.com>
2023-11-25 23:20:19 +0200

    .

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -64,7 +64,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Mary, Ender-3 V1.2)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Mary, Ender-3 V1)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
@@ -132,7 +132,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 V1.2"
+#define CUSTOM_MACHINE_NAME "Ender-3 V1"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
